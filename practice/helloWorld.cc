f0065e242237fe9297b430208292dfa684b64168
Iliaromanov <iliaromanov033@gmail.com>
2021-10-03 10:59:48 -0400

    rename to .cc extension and specifying namespace

---
+++ practice/helloWorld.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+// to avoid having to do std:: in front of cout and endl, we can use
+using std::cout;
+using std::endl;
+// or for all things to use std
+// using namespace std;
+
+int main() {
+    // std::cout << "Hello world!" << std::endl;
+    cout << "Hello world! (but better)" << endl;
+    return 0;
+}
