7b929c146535ff325a3c1d351801fff8d95893b4
Iliaromanov <iliaromanov033@gmail.com>
2021-10-03 18:20:59 -0400

    error handling for cin

--- arbitrarySum.cc
+++ arbitrarySum.cc
@@ -5,12 +5,20 @@ int main() {
     int sum = 0;
     int i;
     while (true) {
-        cin >> i;
-        if (cin.fail()) break;
-        sum += i;
-        cout << "Added: " << i << endl;
-        cout << "Current Sum: " << sum << endl;
+        if (!(cin >> i)) {
+            if (cin.eof()) break;
+
+            // If its not an eof, then clear error and ignore the non-int input
+            cin.clear();
+            cin.ignore();
+        } else {
+            sum += i;
+            cout << "Added: " << i << endl;
+            cout << "Current Sum: " << sum << endl;
+        };
+        
     }
-    cout << "Final Sum: " << sum << endl;
+    cout << "Final Sum: ";
+    cout << sum << endl;
     cout << "Program finished" << endl;
 }
