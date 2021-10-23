a8a2a03ee98623328e61dd59cf079a63df3588e0
Iliaromanov <iliaromanov033@gmail.com>
2021-10-23 10:13:37 -0400

    default constructor param values

--- OOP/struct_based_classes/student_cls/student.h
+++ OOP/struct_based_classes/student_cls/student.h
@@ -1,10 +1,12 @@
+// interface
 #ifndef _STUDENT_H_
 #define _STUDENT_H_
 
 struct Student {
     int assignments, mt, final;
 
-    Student(int assignments, int mt, int final); // constructor
+    Student(int assignments = 0, int mt = 0, int final = 0); // constructor
+    // Note: default parameter values only go in the interface and NOT the implementation
     float grade(); // method
 };
 
