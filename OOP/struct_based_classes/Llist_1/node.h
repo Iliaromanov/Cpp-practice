05322252f121656112166f06a1f9c55c63c881b3
Iliaromanov <iliaromanov033@gmail.com>
2021-10-24 10:35:06 -0400

    copy assignment operator

--- OOP/struct_based_classes/Llist_1/node.h
+++ OOP/struct_based_classes/Llist_1/node.h
@@ -10,6 +10,29 @@ struct Node {
     // Copy constructor that performs deep copy on nodes
     Node(const Node &n): data{n.data},
                          next{n.next ? new Node{*n.next} : nullptr} {}
+    // Copy assignment operator
+    Node &operator=(const Node &other) {
+        if (this == &other) return *this; // check for self assignment
+        Node * tmp = other.next? new Node{ *other.next } : nullptr; // deep copy to tmp, terminates here if not enough heap memory
+        data = other.data;
+        delete next; // free the current contents of this
+        next = tmp; // deep copy of the new contents
+        return *this; // return modified lhs object
+    }
+    // Could alternatively do:
+    /*
+    void swap( Node & other ) {
+      std::swap( data, other.data );
+      std::swap( next, other.next );
+   }
+    Node & operator=( const Node & other) {
+        Node tmp{ other };
+        swap( tmp );
+        return *this;
+    }
+    */
+
+
     // Destructor:
     // invoked whenever delete is called on a Node pointer
     // thus its recursive since it uses delete
